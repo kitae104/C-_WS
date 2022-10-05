6b961230aca3aaf3ea36e41c2ce89371a1d9324d
Kitae's Github <aqua0405@gmail.com>
2022-10-05 18:27:13 +0900

    파일정보 디자인

--- Project2/01_데이터베이스/01_데이터베이스/Form1.Designer.cs
+++ Project2/01_데이터베이스/01_데이터베이스/Form1.Designer.cs
@@ -28,12 +28,160 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listViewFile = new System.Windows.Forms.ListView();
+            this.chName = new System.Windows.Forms.ColumnHeader();
+            this.chDate = new System.Windows.Forms.ColumnHeader();
+            this.chType = new System.Windows.Forms.ColumnHeader();
+            this.chSize = new System.Windows.Forms.ColumnHeader();
+            this.chPath = new System.Windows.Forms.ColumnHeader();
+            this.btnPath = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.rbExcel = new System.Windows.Forms.RadioButton();
+            this.rbTxt = new System.Windows.Forms.RadioButton();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listViewFile
+            // 
+            this.listViewFile.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chName,
+            this.chDate,
+            this.chType,
+            this.chSize,
+            this.chPath});
+            this.listViewFile.FullRowSelect = true;
+            this.listViewFile.GridLines = true;
+            this.listViewFile.Location = new System.Drawing.Point(12, 72);
+            this.listViewFile.Name = "listViewFile";
+            this.listViewFile.Size = new System.Drawing.Size(642, 278);
+            this.listViewFile.TabIndex = 0;
+            this.listViewFile.UseCompatibleStateImageBehavior = false;
+            this.listViewFile.View = System.Windows.Forms.View.Details;
+            // 
+            // chName
+            // 
+            this.chName.Text = "이름";
+            this.chName.Width = 150;
+            // 
+            // chDate
+            // 
+            this.chDate.Text = "수정한 날짜";
+            this.chDate.Width = 100;
+            // 
+            // chType
+            // 
+            this.chType.Text = "유형";
+            this.chType.Width = 100;
+            // 
+            // chSize
+            // 
+            this.chSize.Text = "크기";
+            this.chSize.Width = 100;
+            // 
+            // chPath
+            // 
+            this.chPath.Text = "경로";
+            this.chPath.Width = 250;
+            // 
+            // btnPath
+            // 
+            this.btnPath.Location = new System.Drawing.Point(12, 12);
+            this.btnPath.Name = "btnPath";
+            this.btnPath.Size = new System.Drawing.Size(71, 54);
+            this.btnPath.TabIndex = 1;
+            this.btnPath.Text = "파일";
+            this.btnPath.UseVisualStyleBackColor = true;
+            this.btnPath.Click += new System.EventHandler(this.btnPath_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.rbExcel);
+            this.groupBox1.Controls.Add(this.rbTxt);
+            this.groupBox1.Location = new System.Drawing.Point(101, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(377, 54);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "파일 저장";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(261, 22);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "저장";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // rbExcel
+            // 
+            this.rbExcel.AutoSize = true;
+            this.rbExcel.Location = new System.Drawing.Point(152, 22);
+            this.rbExcel.Name = "rbExcel";
+            this.rbExcel.Size = new System.Drawing.Size(58, 19);
+            this.rbExcel.TabIndex = 1;
+            this.rbExcel.TabStop = true;
+            this.rbExcel.Text = "EXCEL";
+            this.rbExcel.UseVisualStyleBackColor = true;
+            this.rbExcel.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // rbTxt
+            // 
+            this.rbTxt.AutoSize = true;
+            this.rbTxt.Location = new System.Drawing.Point(54, 22);
+            this.rbTxt.Name = "rbTxt";
+            this.rbTxt.Size = new System.Drawing.Size(44, 19);
+            this.rbTxt.TabIndex = 0;
+            this.rbTxt.TabStop = true;
+            this.rbTxt.Text = "TXT";
+            this.rbTxt.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "모든 파일(*.*)|*.*";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "모든 파일(*.*)|*.*";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(666, 362);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnPath);
+            this.Controls.Add(this.listViewFile);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "파일 정보 저장하기";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private ListView listViewFile;
+        private Button btnPath;
+        private GroupBox groupBox1;
+        private Button btnSave;
+        private RadioButton rbExcel;
+        private RadioButton rbTxt;
+        private ColumnHeader chName;
+        private ColumnHeader chDate;
+        private ColumnHeader chType;
+        private ColumnHeader chSize;
+        private ColumnHeader chPath;
+        private OpenFileDialog openFileDialog1;
+        private SaveFileDialog saveFileDialog1;
     }
 }
--- Project2/01_데이터베이스/01_데이터베이스/Form1.cs
+++ Project2/01_데이터베이스/01_데이터베이스/Form1.cs
@@ -1,3 +1,6 @@
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+
 namespace _01_데이터베이스
 {
     public partial class Form1 : Form
@@ -6,5 +9,111 @@ namespace _01_데이터베이스
         {
             InitializeComponent();
         }
+
+        FileInfo fi = null;     //파일 정보 가져오기
+        string FsPath = "";     //파일 경로 저장
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPath_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                foreach (var f in openFileDialog1.FileNames)
+                {
+                    listViewFile.Items.Add(new ListViewItem(new String[]
+                    {
+                        fi.Name, fi.LastWriteTime.ToString(), fi.Extension.Split('.')[1],
+                        GetFileSize(fi.Length), fi.FullName
+                    }));
+                }
+            }
+        }
+
+        private string GetFileSize(double byteCount)
+        {
+            string size = "0 Bytes";
+            if (byteCount >= 1073741824.0)
+                size = String.Format("{0:##.##}", byteCount / 1073741824.0) + " GB";
+            else if (byteCount >= 1048576.0)
+                size = String.Format("{0:##.##}", byteCount / 1048576.0) + " MB";
+            else if (byteCount >= 1024.0)
+                size = String.Format("{0:##.##}", byteCount / 1024.0) + " KB";
+            else if (byteCount > 0 && byteCount < 1024.0)
+                size = byteCount.ToString() + " Bytes";
+
+            return size;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if(listViewFile.Items.Count == 0)
+            {
+                MessageBox.Show("저장할 파일 정보가 없습니다.", "알림", 
+                    MessageBoxButtons.OK, 
+                    MessageBoxIcon.Information);
+                return;
+            }
+
+            if(rbTxt.Checked == true)
+            {
+                saveFileDialog1.Filter = "텍스트 파일(*.txt) | *.txt";
+                if(saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    FsPath = saveFileDialog1.FileName;
+                    TxtSave();
+                }
+            }
+            else
+            {
+                saveFileDialog1.Filter = "엑셀 파일(*.xlsx) | *.xlsx";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    FsPath = saveFileDialog1.FileName;
+                    ExcelSave();
+                }
+            }
+        }
+
+        private void ExcelSave()
+        {
+            
+        }
+
+        private void TxtSave()
+        {
+            using(StreamWriter sw = new StreamWriter(FsPath))
+            {
+                sw.WriteLine("이름" + "\t" + "수정한 날짜" + "\t"
+                    + "유형" + "\t" + "크기" + "\t" + "경로" + "\t");
+
+                for (int i = 0; i < listViewFile.Items.Count; i++)
+                {
+                    String FInfo = "";
+                    for (int j = 0; j < listViewFile.Items[i].SubItems.Count; j++)
+                    {
+                        FInfo += listViewFile.Items[i].SubItems[j].Text + "\t";
+                    }
+                    sw.WriteLine(FInfo);
+                }
+                sw.Close();
+            }
+        }
+
+
+
     }
 }
