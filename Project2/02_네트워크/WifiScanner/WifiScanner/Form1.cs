19b078fa39752076d73ed7df2a8833ca25f137e8
Kitae's Github <aqua0405@gmail.com>
2022-10-12 01:42:14 +0900

    네트워크 설정

---
+++ Project2/02_네트워크/WifiScanner/WifiScanner/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using NativeWifi;
+using System.Threading;
+
+namespace WifiScanner
+{
+    public partial class Form1 : Form
+    {
+        WlanClient wlanClient = new WlanClient();
+        Thread thrAP = null;
+
+        private delegate void OnWifiViewDelegate(bool flags, object[] AddWifi); //델리게이트 개체 생성
+        private OnWifiViewDelegate OnView = null;                               //델리게이트 개체 생성
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OnView = new OnWifiViewDelegate(OnWifiList);
+            thrAP = new Thread(ThreadList);
+            thrAP.Start();
+        }
+
+        private void ThreadList()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnWifiList(bool flags, object[] AddWifi)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
